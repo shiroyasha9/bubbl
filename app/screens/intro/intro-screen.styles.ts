53b9ecac36b749147047b1a9398b103ad775ed20
Anuj <anujjoshi63@gmail.com>
2022-04-14 18:21:50 +0530

    fix intro screen

--- app/screens/intro/intro-screen.styles.ts
+++ app/screens/intro/intro-screen.styles.ts
@@ -1,15 +1,47 @@
 import { StyleSheet } from "react-native"
-import { color, spacing, fontSize, textStyle } from "@theme"
-import { hp } from "@utils"
+import {
+    color, spacing, fontSize,
+    // textStyle
+} from "@theme"
+// import { hp } from "@utils"
 
 export default StyleSheet.create({
+    mainLineContainer: {
+        flex: 0.25,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: spacing[1]
+    },
+    mainLine: {
+        flex: 1,
+        color: color.palette.fontDarkBlue,
+        fontFamily: "Poppins-Bold",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 22,
+        lineHeight: 30,
+        paddingHorizontal: spacing[2]
+    },
+    reasonBox: {
+        height: 135,
+        width: 160,
+        borderRadius: 12,
+        padding: spacing[2],
+        justifyContent: "center",
+        margin: spacing[1],
+    },
+    mascotImageContainer: { flex: 1, width: "100%", justifyContent: "center", alignItems: "center" },
+    mascotImage: {
+        height: "75%",
+    },
+    reasonBoxText: { color: color.palette.fontDarkBlue, textAlign: "center", fontSize: 16 },
     root: {
         alignItems: "center",
         backgroundColor: color.palette.bgGrey,
         color: color.palette.white,
         flex: 1,
         justifyContent: "center",
-        padding: spacing[7],
+        padding: spacing[5],
     },
     body: {
         marginTop: spacing[6],
@@ -22,44 +54,16 @@ export default StyleSheet.create({
     },
     skip: {
         backgroundColor: color.palette.transparent,
-
-        // paddingHorizontal: spacing[4],
-        // paddingVertical: spacing[4],
-    },
-    skipText: {
-
-        color: color.palette.lighterBlack,
-        fontSize: fontSize.regular,
-
-    },
-    detailsText: {
-        ...textStyle.semiBold,
-    },
-    footer: {
-        backgroundColor: color.palette.snowWhite,
-    },
-    footerContent: {
         paddingHorizontal: spacing[2],
         paddingVertical: spacing[2],
         color: "#636363",
         position: "absolute",
         right: spacing[2],
-        top: spacing[2]
+        top: spacing[2],
     },
-    full: { flex: 1 },
-    header: {
-        alignItems: "center",
-        flexDirection: "row",
-        justifyContent: "space-between",
+    skipText: {
+        color: color.palette.lighterBlack,
+        fontSize: fontSize.cta,
     },
-    profilePic: {
-        borderRadius: hp(50),
-        height: hp(40),
-        width: hp(40),
-    },
-    title: {
-        ...textStyle.bold,
-        fontSize: fontSize.h3,
-        lineHeight: 38,
-    },
+
 })
--- app/screens/intro/intro-screen.tsx
+++ app/screens/intro/intro-screen.tsx
@@ -1,80 +1,112 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, View, Image } from "react-native"
+import {
+  // ViewStyle, 
+  View, Image
+} from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
 import { Button, Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-import { color, spacing } from "../../theme"
+import {
+  // color,
+  //  spacing
+} from "../../theme"
 import styles from "./intro-screen.styles"
 
+export const IntroScreen: FC<StackScreenProps<NavigatorParamList, "intro">> = observer(
+  function IntroScreen({ navigation }) {
+    const skipToOnboarding = () => navigation.navigate("welcome")
+
+    return (
+      <Screen style={styles.root} preset="scroll">
 
 
-export const IntroScreen: FC<StackScreenProps<NavigatorParamList, "intro">> = observer(function IntroScreen({ navigation }) {
+        <View style={styles.skip}>
+          <Button
+            testID="skip-screen-button"
+            style={styles.skip}
+            textStyle={styles.skipText}
+            text="skip"
+            onPress={skipToOnboarding}
+          />
+        </View>
 
-  const skipToOnboarding = () => navigation.navigate("welcome")
+        <View style={styles.mascotImageContainer}>
+          <Image
+            source={require("@assets/images/normal.png")}
+            style={styles.mascotImage}
+            resizeMode="contain"
+          />
+        </View>
 
-  return (
-    <Screen style={styles.root} preset="scroll">
-      {/* <Text preset="header" text="intro" /> */}
-      {/* <Text>skipToOnboarding</Text> */}
-      {/* @ts-ignore */}
+        <View
+          style={styles.mainLineContainer}
+        >
+          <Text
+            style={styles.mainLine}
+          >
+            What are you looking to improve?
+          </Text>
+        </View>
 
-
-      <View style={styles.footerContent}>
-
-        <Button
-          testID="next-screen-button"
-          style={styles.skip}
-          textStyle={styles.skipText}
-          text="skip"
-          onPress={skipToOnboarding}
-        />
-      </View>
-
-
-      <View style={{ flex: 1, width: "100%", }}>
-        <Image
-          source={require("@assets/images/normal.png")}
+        <View
           style={{
-            height: '100%'
+            flex: 1,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center"
           }}
-          resizeMode="contain" />
-        {/* <Image source={require("@assets/images/mascot.svg")} style={BOWSER} /> */}
-      </View>
+        >
+          <View
+            style={{
+              ...styles.reasonBox,
+              backgroundColor: "#EDD0FF",
+            }}
+          >
+            <Text style={styles.reasonBoxText}>
+              Sleep better
+            </Text>
+          </View>
 
-      <View style={{
-        flexDirection: "row", flex: 0.5, justifyContent: "center", alignItems: "center",
-      }}>
-        <Text style={{ flex: 1, color: color.palette.fontDarkBlue, fontFamily: "Poppins-Bold", fontSize: 20, justifyContent: "center", }}>
-          What are you looking to improve?
-        </Text>
-      </View >
+          <View
+            style={{
+              ...styles.reasonBox,
+              backgroundColor: "#F4E1FF",
+            }}
+          >
+            <Text style={styles.reasonBoxText}>
+              Manage stress and anxiety
+            </Text>
+          </View>
+          {/* </View> */}
 
-      {/* @ts-ignore */}
-      < View style={{ flex: 1, flexDirection: "row", flexWrap: 'wrap', width: "100%", gap: 16, color: color.palette.fontDarkBlue }}>
-        {/* @ts-ignore */}
-        <View style={{ flexBasis: 'calc(46%)', flex: 1, backgroundColor: "#EDD0FF", borderRadius: 12, height: "45%", padding: spacing[3], textAlign: "center", justifyContent: "center", fontSize: 16 }}>
-          <Text>Sleep better</Text>
-        </View>
-        {/* @ts-ignore */}
-        <View style={{ flexBasis: 'calc(46%)', flex: 1, backgroundColor: "#F4E1FF", borderRadius: 12, height: "45%", padding: spacing[3], textAlign: "center", justifyContent: "center", fontSize: 16 }}>
-          <Text>Manage stress and anxiety</Text>
-        </View>
-        {/* </View> */}
+          <View
+            style={{
+              ...styles.reasonBox,
+              backgroundColor: "#FFD9FF",
+            }}
+          >
+            <Text style={styles.reasonBoxText}>
+              Relax and feel rested
+            </Text>
+          </View>
 
-        {/* <View style={{ flex: 1, backgroundColor: "blue", flexDirection: "row", flexWrap: 'wrap', width: "100%", gap: 10 }}> */}
-        {/* @ts-ignore */}
-        <View style={{ flexBasis: 'calc(46%)', flex: 1, backgroundColor: "#FFD9FF", borderRadius: 12, height: "45%", padding: spacing[3], textAlign: "center", justifyContent: "center", fontSize: 16 }}>
-          <Text>Relax and feel rested</Text>
+          <View
+            style={{
+              ...styles.reasonBox,
+              backgroundColor: "#FFCEFF",
+            }}
+          >
+            <Text style={styles.reasonBoxText}>
+              Self love
+            </Text>
+          </View>
         </View>
-        {/* @ts-ignore */}
-        <View style={{ flexBasis: 'calc(46%)', flex: 1, backgroundColor: "#FFCEFF", borderRadius: 12, height: "45%", padding: spacing[3], textAlign: "center", justifyContent: "center", fontSize: 16 }}>
-          <Text>Self love</Text>
-        </View>
-      </View >
-      {/* <View style={{ flex: 1, backgroundColor: "blue", flexDirection: "row", flexWrap: 'wrap', }}>
+        {/* <View style={{ flex: 1, backgroundColor: "blue", flexDirection: "row", flexWrap: 'wrap', }}>
         <View style={{ flex: 1, backgroundColor: "gray", borderRadius: 12, flexBasis: "calc(50% - 40px)" }}>
           <Text>1</Text>
         </View>
@@ -91,6 +123,7 @@ export const IntroScreen: FC<StackScreenProps<NavigatorParamList, "intro">> = ob
           <Text>4</Text>
         </View>
       </View> */}
-    </Screen >
-  )
-})
+      </Screen>
+    )
+  },
+)
