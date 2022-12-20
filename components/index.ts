a76fc4a8d546ebd8ccecffda88c8b38fcc5607f4
Mubin Ansari <mubinansari910@gmail.com>
2022-12-21 01:06:23 +0530

    make naming scheme consistent for screen component too

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
-export * from "./Screen";
 export * from "./onboarding-flatlist/onboarding-flatlist";
 export * from "./onboarding-footer/onboarding-footer";
 export * from "./primary-button/primary-button";
+export * from "./screen/screen";
