99c469f4acf7e241c470fa9278a2257e3aa51869
Mubin Ansari <mubinansari910@gmail.com>
2022-12-21 00:37:23 +0530

    add poppins font support back

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -7,18 +7,44 @@ import {
 } from "react-native";
 
 import { Colors } from "@themes";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 type ScreenProps = {
   children: React.ReactNode;
 };
+
+SplashScreen.preventAutoHideAsync();
 
 export const Screen: React.FC<ScreenProps & ViewProps> = ({
   children,
   style,
   ...props
 }) => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("@assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Italic": require("@assets/fonts/Poppins-Italic.ttf"),
+    "Poppins-Bold": require("@assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-SemiBold": require("@assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaView style={[styles.root, style]} {...props}>
+    <SafeAreaView
+      style={[styles.root, style]}
+      {...props}
+      onLayout={onLayoutRootView}
+    >
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.container}
