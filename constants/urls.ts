0b38f09e326f41bd93a23262d5273d9429aa8f45
Mubin Ansari <mubinansari910@gmail.com>
2022-12-22 00:11:19 +0530

    add refresh token and revoke token login for google oauth

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,2 +1,7 @@
 export const GOOGLE_FETCH_USER_DATA_URL =
   "https://www.googleapis.com/oauth2/v1/userinfo?alt=json";
+
+export const GOOGLE_REFRESH_TOKEN_URL =
+  "https://www.googleapis.com/oauth2/v4/token";
+
+export const GOOGLE_REVOKE_TOKEN_URL = "https://oauth2.googleapis.com/revoke";
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,19 +1,25 @@
 import { Button, Screen } from "@components";
-import { GOOGLE_FETCH_USER_DATA_URL, IS_EXPO_GO } from "@constants";
+import {
+  GOOGLE_FETCH_USER_DATA_URL,
+  GOOGLE_REFRESH_TOKEN_URL,
+  GOOGLE_REVOKE_TOKEN_URL,
+  IS_EXPO_GO,
+} from "@constants";
 import { ANDROID_OAUTH_ID, EXPO_OAUTH_ID, IOS_OAUTH_ID } from "@env";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { TFetchUserDataResponse } from "@types";
-import type { TokenResponse } from "expo-auth-session";
+import { TokenResponse, refreshAsync, revokeAsync } from "expo-auth-session";
 
 import * as Google from "expo-auth-session/providers/google";
 import { useEffect, useState } from "react";
-import { Image, StyleSheet, Text } from "react-native";
+import { Image, Platform, StyleSheet, Text } from "react-native";
 
 export const Home = () => {
   const [userInfo, setUserInfo] = useState<TFetchUserDataResponse | null>(null);
   const [auth, setAuth] = useState<TokenResponse | null>(null);
+  const [requireRefresh, setRequireRefresh] = useState(false);
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
+  const [_, response, promptAsync] = Google.useAuthRequest({
     androidClientId: ANDROID_OAUTH_ID,
     iosClientId: IOS_OAUTH_ID,
     expoClientId: EXPO_OAUTH_ID,
@@ -37,7 +43,10 @@ export const Home = () => {
     const getPersistedAuth = async () => {
       const auth = await AsyncStorage.getItem("auth");
       if (auth !== null) {
-        setAuth(JSON.parse(auth));
+        const authObj = JSON.parse(auth) as TokenResponse;
+        setAuth(authObj);
+
+        setRequireRefresh(!TokenResponse.isTokenFresh(authObj));
       }
     };
     getPersistedAuth();
@@ -52,6 +61,55 @@ export const Home = () => {
     setUserInfo((await data.json()) as TFetchUserDataResponse);
   };
 
+  const getClientId = () => {
+    if (IS_EXPO_GO) {
+      return EXPO_OAUTH_ID;
+    }
+    if (Platform.OS === "ios") {
+      return IOS_OAUTH_ID;
+    }
+    return ANDROID_OAUTH_ID;
+  };
+
+  const refreshToken = async () => {
+    const clientId = getClientId();
+    const refreshToken = auth?.refreshToken;
+    const tokenResult = await refreshAsync(
+      {
+        clientId,
+        refreshToken,
+      },
+      { tokenEndpoint: GOOGLE_REFRESH_TOKEN_URL },
+    );
+
+    setAuth(tokenResult);
+    await AsyncStorage.setItem("auth", JSON.stringify(tokenResult));
+    setRequireRefresh(false);
+  };
+
+  const logoutHandler = async () => {
+    await revokeAsync(
+      {
+        token: auth?.accessToken!,
+      },
+      {
+        revocationEndpoint: GOOGLE_REVOKE_TOKEN_URL,
+      },
+    );
+    setAuth(null);
+    setUserInfo(null);
+    await AsyncStorage.removeItem("auth");
+  };
+
+  if (requireRefresh) {
+    return (
+      <Screen style={styles.root}>
+        <Text>Needs Refresh</Text>
+        <Button text="Refresh token" onPress={refreshToken} />
+      </Screen>
+    );
+  }
+
   return (
     <Screen style={styles.root}>
       <Text>Home</Text>
@@ -65,12 +123,16 @@ export const Home = () => {
       )}
       <Button
         text={auth ? "Get User Data" : "Sign In"}
+        style={styles.button}
         onPress={
           auth
             ? getUserData
             : () => promptAsync({ useProxy: IS_EXPO_GO, showInRecents: true })
         }
       />
+      {auth && (
+        <Button text="Sign Out" onPress={logoutHandler} style={styles.button} />
+      )}
     </Screen>
   );
 };
@@ -85,5 +147,8 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
+  },
+  button: {
+    marginVertical: 10,
   },
 });
